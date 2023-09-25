de1839e73c8a2aefdb66a3f693a95cb25c989c67
codyivy0 <codyivy0@gmail.com>
2023-09-25 00:22:51 -0500

    form state

--- src/App.js
+++ src/App.js
@@ -14,21 +14,38 @@ function App() {
   }
 
   function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
 
     function handleSubmit(e) {
-      e.preventDefault()
+      e.preventDefault();
+
+      if(!description) return
+
+      const newItem = { description, quantity, packed: false, id: Date.now() };
+      
+      setDescription('')
+      setQuantity(1)
+    }
+    function handleChange(e) {
+      setDescription(e.target.value);
     }
     return (
       <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your trip?😍</h3>
-        <select>
+        <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
           {Array.from({ length: 20 }, (v, i) => i + 1).map((num) => (
             <option value={num} key={num}>
               {num}
             </option>
           ))}
         </select>
-        <input type="text" placeholder="Item..." />
+        <input
+          value={description}
+          type="text"
+          placeholder="Item..."
+          onChange={handleChange}
+        />
         <button>Add</button>
       </form>
     );
@@ -49,7 +66,7 @@ function App() {
       <div className="list">
         <ul>
           {initialItems.map((item) => (
-            <Item item={item} key={item.id}/>
+            <Item item={item} key={item.id} />
           ))}
         </ul>
       </div>
