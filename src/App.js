7c953b2d418c86ec187689852887c5ee069c3156
codyivy0 <codyivy0@gmail.com>
2023-09-24 23:21:00 -0500

    Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useState} from 'react'
 
-function App() {
+function App() { 
+  function Logo() {
+    return <h1>🌴 Far Away 🌴</h1>
+  }
+
+  function Form() {
+    <div className="add-form">
+      
+    </div>
+  }
+  function PackingList() {}
+  function Stats() {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
